6597de2034e14e9539a852c95056e8b0161d0198
sirajjunior540 <abdullah@kmmrce.com>
2023-05-23 16:03:27 +0400

    change database

--- sudan249/settings.py
+++ sudan249/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'sudan249.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sudancommunity',  # The name of the PostgreSQL database you created
+        'USER': 'postgres',  # The username for the PostgreSQL user
+        'PASSWORD': 'BlackPharaoh249',  # The password for the PostgreSQL user
+        'HOST': 'database.cb0zf62ayijv.us-east-1.rds.amazonaws.com',  # This should match the service name for PostgreSQL in your docker-compose.yml file
+        'PORT': '5432',  # The default PostgreSQL port
     }
 }
 
